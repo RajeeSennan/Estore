d47b795051a54796c21241c086a40a4b6d9e1489
Rajee <RajeeSennan@gmail.com>
2023-04-02 16:32:21 -0500

    Adding images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
+import data from './data';
 function App() {
   return (
     <div>
       <header>
         <a href="/">eStore for Books</a>
       </header>
-      <main>list books</main>
+      <main>
+        <h1>featured Products</h1>
+        <div className="books">
+          {data.books.map((book) => (
+            <div className="book" key={book.id}>
+              <a href={`/book/${book.id}`}>
+                <img src={book.image} alt={book.name} />
+              </a>
+              <div className="book-info">
+                <a href={`/book/${book.id}`}>
+                  <p>{book.name}</p>
+                </a>
+                <p>
+                  <strong>${book.price}</strong>
+                </p>
+              </div>
+              <button>Add to cart</button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
