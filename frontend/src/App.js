4837f0c00a586b73d8bae74a24effabd61c0077f
Rajee <RajeeSennan@gmail.com>
2023-04-02 15:25:02 -0500

    video-03-create app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">eStore for Books</a>
+      </header>
+      <main>list books</main>
+    </div>
+  );
+}
+
+export default App;
