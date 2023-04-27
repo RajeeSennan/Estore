b5efad61c1f2978668207104e4db77c37686de07
Rajee <RajeeSennan@gmail.com>
2023-04-27 05:22:52 -0500

    Commiting PlaceOrder Code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
 import BookScreeen from './screens/BookScreen';
 import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
@@ -13,19 +14,25 @@ import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart, userInfo } = state;
 
-  const signoutHandler = () =>{
-    ctxDispatch ({type: 'USER_SIGNOUT'});
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
-  }
+    localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
+  };
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
-        <ToastContainer position='bottom-center' limit={1} />
+        <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
@@ -42,24 +49,27 @@ function App() {
                   )}
                 </Link>
                 {userInfo ? (
-                  <NavDropdown title={userInfo.name} id='basic-nav-dropdown' >
-                    <LinkContainer to='/profile'>
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
                       <NavDropdown.Item>User Profile </NavDropdown.Item>
                     </LinkContainer>
-                    <LinkContainer to='/orderhistory'>
+                    <LinkContainer to="/orderhistory">
                       <NavDropdown.Item>Order History </NavDropdown.Item>
                     </LinkContainer>
                     <NavDropdown.Divider />
-                    <Link 
-                    className='dropdown-item' to='#signout' onClick={signoutHandler}>
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
                       Sign Out
                     </Link>
                   </NavDropdown>
-                ): (
-                  <Link className="nav-link" to='/signin'>
-                  Sign In
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
                   </Link>
-                ) }
+                )}
               </Nav>
             </Container>
           </Navbar>
@@ -70,6 +80,10 @@ function App() {
               <Route path="/book/:slug" element={<BookScreeen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
+              <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
